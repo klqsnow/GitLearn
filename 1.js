c10c9b680a9631ca867b4b47ee19fbf0df603174
孔令卿 <klqsnow@163.com>
2021-02-03 14:23:35 +0800

    update 1.js feature1

--- 1.js
+++ 1.js
@@ -1,4 +1,13 @@
-let a = 10;
+let a = 10; let b = 100,
 console.log(a);
+console.log(a, b);
 console.log('a');
+console.log('kkkk1');
 console.log('kkkk');
+console.log('kkkk1');
+
+console.log('kkkk1');
+console.log('kkkk1');
+console.log('kkkk1');
+console.log('kkkk1');
+
